973d7396baa172256b9a267bf30117089f114ed7
Sam Zajczenko <113551869+Sam786968@users.noreply.github.com>
2022-10-03 19:21:45 +0000

    Initial commit

--- main.py
+++ main.py
@@ -9,9 +9,31 @@
 #importing modules
 #work on creating and understanding functions
 
-from decision_making import decisions
 from comparison_operators import comparison
+from decision_making import decisions
+from dictionaries_loops import dictionaries_loops
+from enumerators import num1
+from list_comprehension import num2
+from logical_operstors import num3
+from loops import num4
+from min_max import num5
+from random import num6
+from ranges import num7
+from while_loops import num8
+from zip import num9
 
 decisions()
 comparison()
+dictionaries_loops()
+num1()
+num2()
+num3()
+num4()
+num5()
+num6()
+num7()
+num8()
+num9()
 
+
+# This moulation is a great way to work on multiple pieces of code at once and be ablte to test sections of it, instead of having to comment code or make sure all of it works.
